58ecdc4ff923d4a385dfa678b554b9097cef62c5
amir <mail.hampacredit.com>
2023-11-18 10:08:38 +0330

    some changes

--- src/app/ui-kit/custom-jalali-date-picker/custom-jalali-date-picker.component.ts
+++ src/app/ui-kit/custom-jalali-date-picker/custom-jalali-date-picker.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, Self, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, Self} from '@angular/core';
 import {FormControl, NgControl, ReactiveFormsModule} from "@angular/forms";
 import {BaseControlValueAccessor} from "../../utils/BaseControlValueAccessor";
 import {NgClass} from "@angular/common";
@@ -9,61 +9,31 @@ import {NgPersianDatepickerModule} from 'ng-persian-datepicker';
   standalone: true,
   imports: [NgPersianDatepickerModule, ReactiveFormsModule, NgClass],
   template: `
-    <div class="w-full uikit-wrapper-height" [ngClass]="class">
-      <div class="flex align-items-stretch">
-        <div class="flex-grow-1">
-          <ng-persian-datepicker
-            [dateInitValue]="!!value"
-            [timeEnable]="timeEnable"
-            [timeShowSecond]="timeEnable"
-            [dateFormat]="timeEnable ? 'YYYY/MM/DD HH:mm:ss' :'YYYY/MM/DD'"
-            [dateGregorianFormat]="timeEnable ? 'YYYY/MM/DD HH:mm:ss' :'YYYY/MM/DD'">
-            <span class="p-float-label w-full">
-              <input
-                [formControl]="control"
-                type="text"
-                #input
-                [value]="value"
-                [disabled]="disabled"
-                [ngClass]="{'ng-invalid ng-dirty' : control.invalid &&( control.dirty || control.touched)}"
-                [style]="{'width':'100%'}"
-                (input)="onChanged($event)"
-                (blur)="touched()"/>
-                <label class="text-1 font-sm-regular">{{label}}</label>
-            </span>
-          </ng-persian-datepicker>
-        </div>
-      </div>
+    <div class="w-full" [ngClass]="class">
+      <ng-persian-datepicker
+        [dateInitValue]="!!value">
+        <input
+          [formControl]="control"
+          type="text"
+          #input
+          [value]="value"
+          [disabled]="disabled"
+          (input)="onChanged($event)"
+          (blur)="touched()"/>
+      </ng-persian-datepicker>
     </div> `,
   styles: [`
     :host ::ng-deep .datepicker-outer-container {
       position: fixed;
       z-index: 10000;
-    }
-
-    :host ::ng-deep .p-inputtext {
-      border-radius: 0 6px 6px 0;
-    }
-
-    .icon {
-      background-color: #3B82F6;
-      border-radius: 6px 0 0 6px;
-      width: 44px;
-
-      i {
-        color: white;
-      }
     } `]
 })
 export class CustomJalaliDatePickerComponent extends BaseControlValueAccessor<string> implements OnInit {
 
   control: FormControl
 
-  @Input() timeEnable = false;
-  @Input() clearable = true;
   @Input() class: string;
   @Input() label: string;
-  @ViewChild('input') input: ElementRef;
 
   constructor(
     @Self() public controlDir: NgControl
